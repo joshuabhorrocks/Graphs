cd898117e5d6908c786dbe4cfe77c530ebe9cf67
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-07-16 17:30:57 -0600

    Reached MVP for Spint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,9 +5,21 @@ from world import World
 import random
 from ast import literal_eval
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 # Load world
 world = World()
-
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -22,12 +34,52 @@ world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
+### END WORLD GEN ###
 
+
+
+### START PLAYER FUNCTIONALITY ###
+total_rooms = len(room_graph)
+# print("Total Rooms: ", total_rooms)
 player = Player(world.starting_room)
-
+room_info = player.current_room
+explored_rooms = 1
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+visited = set()
+
+path = Stack()
+path.push(0)
+
+
+while explored_rooms < total_rooms:
+    next_move = Stack()
+    current_room = path.stack[-1]
+
+    visited.add(current_room)
+
+    available_dirs = room_graph[current_room][1]
+
+    for move, next_room in available_dirs.items():
+        if next_room not in visited:
+            next_move.push(next_room)
+
+    if next_move.size() > 0:
+        room = next_move.stack[0]
+        path.push(room)
+        explored_rooms += 1
+    else:
+        room = path.stack[-2]
+        path.pop()
+
+    for move, next_room in available_dirs.items():
+        if next_room == room:
+            traversal_path.append(move)
+            
+print("Total Rooms: ", total_rooms)
+print("Total Explored Rooms: ", explored_rooms)
+
 
 
 
@@ -37,6 +89,7 @@ player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
 
 for move in traversal_path:
+    print("Move: ", move)
     player.travel(move)
     visited_rooms.add(player.current_room)
 
@@ -51,12 +104,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- projects/adventure/world.py
+++ projects/adventure/world.py
@@ -8,19 +8,25 @@ class World:
         self.rooms = {}
         self.room_grid = []
         self.grid_size = 0
+
+
     def load_graph(self, room_graph):
         num_rooms = len(room_graph)
         rooms = [None] * num_rooms
         grid_size = 1
+
         for i in range(0, num_rooms):
             x = room_graph[i][0][0]
             grid_size = max(grid_size, room_graph[i][0][0], room_graph[i][0][1])
             self.rooms[i] = Room(f"Room {i}", f"({room_graph[i][0][0]},{room_graph[i][0][1]})",i, room_graph[i][0][0], room_graph[i][0][1])
+        
         self.room_grid = []
         grid_size += 1
         self.grid_size = grid_size
+
         for i in range(0, grid_size):
             self.room_grid.append([None] * grid_size)
+
         for room_id in room_graph:
             room = self.rooms[room_id]
             self.room_grid[room.x][room.y] = room
@@ -32,23 +38,30 @@ class World:
                 self.rooms[room_id].connect_rooms('e', self.rooms[room_graph[room_id][1]['e']])
             if 'w' in room_graph[room_id][1]:
                 self.rooms[room_id].connect_rooms('w', self.rooms[room_graph[room_id][1]['w']])
+
         self.starting_room = self.rooms[0]
+
 
     def print_rooms(self):
         rotated_room_grid = []
+
         for i in range(0, len(self.room_grid)):
             rotated_room_grid.append([None] * len(self.room_grid))
+
         for i in range(len(self.room_grid)):
             for j in range(len(self.room_grid[0])):
                 rotated_room_grid[len(self.room_grid[0]) - j - 1][i] = self.room_grid[i][j]
+
         print("#####")
         str = ""
         for row in rotated_room_grid:
             all_null = True
+
             for room in row:
                 if room is not None:
                     all_null = False
                     break
+
             if all_null:
                 continue
             # PRINT NORTH CONNECTION ROW
@@ -83,6 +96,7 @@ class World:
                 else:
                     str += "     "
             str += "#\n"
+            
         print(str)
         print("#####")
 
